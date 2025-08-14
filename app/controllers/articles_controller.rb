0b7b9632edf071f131396aad2a88bf7ce01cf736
杉本真生 <sugimotomasao@sugimotomasaonoMacBook-Air.local>
2025-08-14 15:58:31 +0900

    記事投稿画面の作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,28 @@
 class ArticlesController < ApplicationController
+  before_action :require_login, only: %i[new create]
+
   def index
-    @articles = Article.includes(:user, image_attachment: :blob).order(created_at: :desc)
+    @articles = Article
+      .includes(:user)
+      .with_attached_image
+      .order(created_at: :desc)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to articles_path, notice: "記事を投稿しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :kind, :extra_info, :image, :tag_names)
   end
 end
