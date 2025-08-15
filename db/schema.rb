0c2a03d37edd95d14d693bd6438fe11ec8c919b1
杉本真生 <sugimotomasao@sugimotomasaonoMacBook-Air.local>
2025-08-15 12:11:15 +0900

    記事投稿画面の調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_14_053515) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_14_140620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,9 +47,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_14_053515) do
     t.text "body"
     t.integer "kind"
     t.text "extra_info"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "guest_name"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
